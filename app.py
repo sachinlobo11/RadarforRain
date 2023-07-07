fe12c0dffe5a835156aae5840d523aaca7907def
Sachin lobo <30945603+sachinlobo11@users.noreply.github.com>
2023-07-07 18:24:03 +0530

    Create app.py
    Initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+import urllib.request
+cap = cv2.VideoCapture(0)
+
+while(1):
+   
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    
+    lower_red = np.array([30,150,50])
+    upper_red = np.array([255,255,180])
+    
+    mask = cv2.inRange(hsv, lower_red, upper_red)
+    res = cv2.bitwise_and(frame,frame, mask= mask)
+
+    cv2.imshow('frame',frame)
+    cv2.imshow('mask',mask)
+    cv2.imshow('res',res)
