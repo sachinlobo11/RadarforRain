a49105d5ab111b318abdc70937ba2fbdcfe7e4d3
Sachin lobo <30945603+sachinlobo11@users.noreply.github.com>
2024-08-14 18:33:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,18 @@ def home():
     cloudinary.uploader.upload("https://mausam.imd.gov.in/Radar/ppz_goa.gif", 
     public_id = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M"))
    
-    return "OK"
+    return '''[
+{
+"book": "John",
+"chapter": "1",
+"verse": "1-4"
+},
+{
+"book": "John",
+"chapter": "1",
+"verse": "1-4"
+}
+]'''
 
     
 
